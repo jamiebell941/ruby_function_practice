16f9dc345324d5cf723a9f1f10ebcb413d97e59a
jamiebell941 <jamiebell941@gmail.com>
2020-02-18 17:18:30 +0000

    completed months

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -42,6 +42,27 @@ def add_string_as_number(num_x, num_y)
 return result
 end
 
+def number_to_full_month_name(month_number)
+
 months = {"January" => 1, "Febuary" => 2, "March" => 3,
   "April" => 4, "May" => 5, "June" => 6, "July" => 7, "August" => 8,
 "September" => 9, "October" => 10, "November" => 11, "December" => 12}
+
+  months.each do| name, number|
+      if month_number == number
+        return name
+      end
+    end
+end
+
+def number_to_short_month_name(month_number)
+months = {"Jan" => 1, "Feb" => 2, "Mar" => 3,
+  "Apr" => 4, "May" => 5, "Jun" => 6, "Jul" => 7, "Aug" => 8,
+"Sep" => 9, "Oct" => 10, "Nov" => 11, "Dec" => 12}
+
+  months.each do |name, number|
+      if month_number == number
+      return name
+      end
+    end
+end
